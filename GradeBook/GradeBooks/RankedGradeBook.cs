e3f43d0b71d508564685035fe666a656c44f97d4
Bongani Nqubuka <dabobongg@gmail.com>
2018-03-15 11:02:38 +0200

    Override RankedGradeBook's GetLetterGrade
    Override RankedGradeBook's GetLetterGrade task 1 to 5.

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -10,5 +10,21 @@ namespace GradeBook.GradeBooks
         {
             Type = Enums.GradeBookType.Ranked;
         }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+                throw new InvalidOperationException();
+
+            if (averageGrade >= 90)
+                return 'A';
+            else if (averageGrade < 90 && averageGrade >= 75)
+                return 'B';
+            else if (averageGrade < 75 && averageGrade >= 50)
+                return 'C';
+            else if (averageGrade < 50)
+                return 'D';
+            return 'F';
+        }
     }
 }
